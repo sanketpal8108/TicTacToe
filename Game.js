0fa7e21ca7a12a0238cd7f6c97a78dfe8371a3d2
sanketpal8108 <83400707+sanketpal8108@users.noreply.github.com>
2023-08-26 00:13:54 +0530

    v2

--- Game.js
+++ Game.js
@@ -1,59 +1,82 @@
 const Board = require("./Board")
 const Player = require("./Player")
 
-class Game{
-    constructor(player1Name , player2Name){
-        this.players = [new Player(player1Name ) , new Player(player2Name)]
-        this.isGameEnded = false
-        this.turn =0
-        this.board = new Board()
+class Game {
+    #players
+    #isGameEnded
+    #turn 
+    #board
+    #winner
+    constructor(player1Name, player2Name) {
+        this.#players = [new Player(player1Name,'X'), new Player(player2Name,'O')]
+        this.#isGameEnded = false
+        this.#turn = 0
+        this.#board = new Board()
+        this.#winner = null
     }
-    static newGame(player1Name,player2Name){
-        
-        if(player1Name==player2Name || typeof player1Name !='string' || typeof player2Name != 'string'){
-            return ['Enter Valid Input' , null]
-        }
-        return ['Object Created ' , new Game(player1Name,player2Name)]
-    }
-    play(cellNumber){
-        if(typeof cellNumber != 'number'){
-            return 'Invalid Inputs'
-        }
-        if(this.isGameEnded){
-            return 'Game Ended , Start New Game'
-        }
-        if(!this.board.isCellEmpty(cellNumber)){
-            return 'Cell is not Empty'
-        }
-        
-        let currentPlayer
-        if(this.turn%2==0){
-            currentPlayer = this.players[0]
-            currentPlayer.setSymbol('X')
-        }
-        else{
-            currentPlayer = this.players[1]
-            currentPlayer.setSymbol('O')
-        }
-        
-        this.board.markCell(cellNumber,currentPlayer.getSymbol())
-        //currentPlayer.markCell(this.board.cells[cellNumber] )
-        //currentPlayer.
-        this.turn++
-        this.board.printBoard()
-        //let gameState = this.board.gameState()
-        if(this.board.isDraw()){
-            this.isGameEnded=true           
-            return 'Game Draw '
-        }
-        else if(this.board.isWon()){
-            this.isGameEnded = true            
+    static newGame(player1Name, player2Name) {
+        try {
+            if (player1Name == player2Name || typeof player1Name != 'string' || typeof player2Name != 'string') {
+                
+                throw new Error ('Enter Valid Input')
+                
+            }
             
-            return `Game Over , Winner is: ${currentPlayer.name}`
+            return new Game(player1Name, player2Name)
+        } catch (error) {
+            console.log(error.message);
         }
-        
-        return 'Continue Playing'
-         
+
+       
+    }
+    play(cellNumber) {
+        try {
+            if (typeof cellNumber != 'number') {
+                
+                throw new Error('Invalid Inputs')
+            }
+            if (this.#isGameEnded) {
+                if (this.#winner != null) {
+    
+                    return `game ended : Winner was ${this.#winner.name}`
+                }
+                return `game ended : DRAW`
+            }
+            if (!this.#board.isCellEmpty(cellNumber)) {
+                
+                throw new Error('Cell is not Empty')
+            }
+    
+            let currentPlayer
+            if (this.#turn % 2 == 0) {
+                currentPlayer = this.#players[0]
+                
+            }
+            else {
+                currentPlayer = this.#players[1]
+                
+            }
+    
+            this.#board.markCell(cellNumber, currentPlayer.getSymbol())
+            
+            this.#turn++
+            this.#board.printBoard()
+            
+            if (this.#board.isDraw()) {
+                this.#isGameEnded = true
+                return 'Game Draw '
+            }
+            else if (this.#board.isWon()) {
+                this.#isGameEnded = true
+               this.#winner = currentPlayer
+                return `Game Over , Winner is: ${this.#winner.name}`
+            }
+    
+            return 'Continue Playing'
+        } catch (error) {
+            console.log(error.message);
+        }
+
     }
 }
 
